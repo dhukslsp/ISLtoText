85edab5b296f1a0686552b82bb9e1e45ca9fed8c
Dhruv Singh (dhukslsp) <dhruv1647.singh@gmail.com>
2024-09-06 12:46:27 +0530

    Expo Camera Adding Started

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,43 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Text, Dimensions, StatusBar,View } from 'react-native';
+import { useRef, useCallback, useMemo } from 'react';
 import { HelloWave } from '@/components/HelloWave';
+// import { Bottom } from '@/components/AppComponents/Bottomsheet';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Camera } from 'expo-camera';
 export default function HomeScreen() {
+  //Code for adding the Scroll View in our App basic configurations
+  const sheetStartSnap = 200;
+  const { height: windowHeight } = Dimensions.get('window');
+  const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const { top, bottom } = useSafeAreaInsets();
+  const sheetEndSnap = windowHeight - (top + bottom + statusBarHeight);
+  const snapPoints = useMemo(() => [sheetStartSnap, sheetEndSnap], [sheetEndSnap]);
+  //End in code for adding Scroll view
+  //Basic code for having camera in our app
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <>
+    <View><Text>Thisi s s</Text></View>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheet
+          ref={bottomSheetRef}
+          onChange={handleSheetChanges}
+          snapPoints={snapPoints}
+        >
+          <BottomSheetView style={styles.contentContainer}>
+            <ThemedText type='defaultSemiBold' >The text generated Will be shown here</ThemedText>
+          </BottomSheetView>
+        </BottomSheet>
+      </GestureHandlerRootView>
+      </>
   );
 }
 
@@ -66,5 +57,14 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
 });
